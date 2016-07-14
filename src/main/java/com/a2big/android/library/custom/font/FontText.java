d40d70170ff85b66763f9d4628e3e76e91ea2668
a2big <a2big@a2bigui-MacBook-Pro.local>
2016-07-14 23:16:44 +0900

    modify FontText.java

--- src/main/java/com/a2big/android/library/custom/font/FontText.java
+++ src/main/java/com/a2big/android/library/custom/font/FontText.java
@@ -6,44 +6,49 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.TextView;
-
-
-
 import org.apache.commons.lang3.StringUtils;
 
 
 public class FontText extends TextView {
+
+    private static final int FONT_DEFAULT = 0;
+    private static final int FONT_1      = 1;
+
+    private static final String FONT_NAME_DEFAULT = "The170.ttf";
+    private static final String FONT_NAME_1 = "The130.ttf";
+
+
     public FontText(Context context) {
         super(context);
 
-        applyCustomFont(context,0);
+        applyCustomFont(context,FONT_DEFAULT);
     }
 
     public FontText(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        applyCustomFont(context,1);
+        applyCustomFont(context,FONT_1);
     }
 
     public FontText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        applyCustomFont(context,1);
+        applyCustomFont(context,FONT_1);
     }
 
     private void applyCustomFont(Context context,int type) {
         Typeface customFont;
         switch(type){
-            case 0:
-                customFont = FontCache.getTypeface("The170.ttf", context);
+            case FONT_DEFAULT:
+                customFont = FontCache.getTypeface(FONT_NAME_DEFAULT, context);
                 setTypeface(customFont);
                 break;
-            case 1:
-                customFont = FontCache.getTypeface("The130.ttf", context);
+            case FONT_1:
+                customFont = FontCache.getTypeface(FONT_NAME_1, context);
                 setTypeface(customFont);
                 break;
             default:
-                customFont = FontCache.getTypeface("The170.ttf", context);
+                customFont = FontCache.getTypeface(FONT_NAME_DEFAULT, context);
                 setTypeface(customFont);
                 break;
         }
